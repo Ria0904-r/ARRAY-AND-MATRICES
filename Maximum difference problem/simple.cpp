a390c4be1eda12dfe189cea5ee46fad1bb55295c
Ria Sehgal <riasehgal48@gmail.com>
2021-12-17 10:12:05 +0530

    Create simple.cpp

---
+++ Maximum difference problem/simple.cpp
@@ -0,0 +1,41 @@
+// C++ program to find Maximum difference
+// between two elements such that larger
+// element appears after the smaller number
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* The function assumes that there are
+   at least two elements in array. The
+   function returns a negative value if the
+   array is sorted in decreasing order and 
+   returns 0 if elements are equal */
+int maxDiff(int arr[], int arr_size)
+{    
+  int max_diff = arr[1] - arr[0];
+  for (int i = 0; i < arr_size; i++)
+  {
+    for (int j = i+1; j < arr_size; j++)
+    {    
+      if (arr[j] - arr[i] > max_diff)
+        max_diff = arr[j] - arr[i];
+    }
+  }        
+  return max_diff;
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+  
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+      cin>>arr[i];
+  }
+  // Function calling
+  cout << "Maximum difference is " << maxDiff(arr, n);
+ 
+  return 0;
+}
